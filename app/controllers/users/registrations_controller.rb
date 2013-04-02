2880298285017f7cd1d4a9459f53c41c7475abda
Olivier <oleblond@allintel.com>
2013-04-02 08:18:23 -0400

    Update

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,14 +3,20 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # redirect after sign-up
 
   def create
-    if verify_recaptcha
-      super
+    # first validate form
+    build_resource
+    if !resource.valid?
+      clean_up_passwords resource
+      respond_with resource
     else
-      build_resource
-      clean_up_passwords(resource)
-      flash.now[:alert] = "There was an error with the recaptcha code below. Please re-enter the code."
-      flash.delete :recaptcha_error
-      render :new
+      if verify_recaptcha # validate re-captcha
+        super
+      else
+        clean_up_passwords(resource)
+        flash.now[:alert] = "There was an error with the recaptcha code below. Please re-enter the code."
+        flash.delete :recaptcha_error
+        render :new
+      end
     end
   end
 
