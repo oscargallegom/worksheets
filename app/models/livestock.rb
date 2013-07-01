e135c571e0aeb272f9829384b3699edd5e333324
Olivier <oleblond@allintel.com>
2013-06-30 22:34:09 -0400

    Bug with animal  units (missing :)

--- app/models/livestock.rb
+++ app/models/livestock.rb
@@ -5,5 +5,5 @@ class Livestock < ActiveRecord::Base
   attr_accessible :animal_id, :animal_units, :farm_id
 
   validates_presence_of :animal_id, :message => '^Select an animal'
-  validates_numericality_of  animal_units, :greater_than_or_equal_to => 0, :message => '^Animal units is not a number'
+  validates_numericality_of  :animal_units, :greater_than_or_equal_to => 0, :message => '^Animal units is not a number'
 end
