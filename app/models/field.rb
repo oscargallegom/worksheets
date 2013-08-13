d769c9c6319712e0ca7424fb4f345351634d92a6
Olivier <oleblond@allintel.com>
2013-08-12 21:41:07 -0400

    Bug fixes.

--- app/models/field.rb
+++ app/models/field.rb
@@ -137,25 +137,25 @@ class Field < ActiveRecord::Base
 
   def n_baseline
     n_baseline = nil
-    n_baseline = self.watershed_segment[:n_crop_baseline] if field_type_id == 1
-    n_baseline = self.watershed_segment[:n_pasture_baseline] if field_type_id == 2
-    n_baseline = self.watershed_segment[:n_hay_baseline] if field_type_id == 3
+    n_baseline = self.watershed_segment[:n_crop_baseline].to_f if field_type_id == 1
+    n_baseline = self.watershed_segment[:n_pasture_baseline].to_f if field_type_id == 2
+    n_baseline = self.watershed_segment[:n_hay_baseline].to_f if field_type_id == 3
     return n_baseline
   end
 
   def p_baseline
     p_baseline = nil
-    p_baseline = self.watershed_segment[:p_crop_baseline] if field_type_id == 1
-    p_baseline = self.watershed_segment[:p_pasture_baseline] if field_type_id == 2
-    p_baseline = self.watershed_segment[:p_hay_baseline] if field_type_id == 3
+    p_baseline = self.watershed_segment[:p_crop_baseline].to_f if field_type_id == 1
+    p_baseline = self.watershed_segment[:p_pasture_baseline].to_f if field_type_id == 2
+    p_baseline = self.watershed_segment[:p_hay_baseline].to_f if field_type_id == 3
     return p_baseline
   end
 
   def sediment_baseline
     sediment_baseline = nil
-    sediment_baseline = self.watershed_segment[:sediment_crop_baseline] / 2000.0 if field_type_id == 1
-    sediment_baseline = self.watershed_segment[:sediment_pasture_baseline] / 2000.0 if field_type_id == 2
-    sediment_baseline = self.watershed_segment[:sediment_hay_baseline] / 2000.0 if field_type_id == 3
+    sediment_baseline = self.watershed_segment[:sediment_crop_baseline].to_f / 2000.0 if field_type_id == 1
+    sediment_baseline = self.watershed_segment[:sediment_pasture_baseline].to_f / 2000.0 if field_type_id == 2
+    sediment_baseline = self.watershed_segment[:sediment_hay_baseline].to_f / 2000.0 if field_type_id == 3
     return sediment_baseline
   end
 
