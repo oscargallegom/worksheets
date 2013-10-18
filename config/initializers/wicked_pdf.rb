65eabafbe324ecfc7b9d4b9f50932b4f71e87a30
Olivier <oleblond@allintel.com>
2013-10-17 22:42:36 -0400

    PDF option

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,7 +1,9 @@
-if Rails.env.staging? || Rails.env.production?
-  exe_path = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
+WickedPdf.config do |config|
+  if Rails.env.staging? || Rails.env.production?
+    config.exe_path = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
 else
-  exe_path = 'C:\Program Files\wkhtmltopdf\wkhtmltopdf.exe'
+  config.exe_path = 'C:\Program Files\wkhtmltopdf\wkhtmltopdf.exe'
 end
+  end
 
 
