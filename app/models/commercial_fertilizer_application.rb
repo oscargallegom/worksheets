66f592c224a605ae5a01e75ed8556ff1f4e1b06d
Olivier <oleblond@allintel.com>
2013-12-20 16:38:24 -0500

    Bug fixes.

--- app/models/commercial_fertilizer_application.rb
+++ app/models/commercial_fertilizer_application.rb
@@ -13,7 +13,7 @@ class CommercialFertilizerApplication < ActiveRecord::Base
 
   validates_presence_of :total_n_applied, :if => 'total_p_applied.nil?', :message => '^You must enter a value for total N applied or total P applied (commercial fertilizer applications section).'
   validates_presence_of :total_p_applied, :if => 'total_n_applied.nil?', :message => '^'
-  validates_presence_of :p_type_id, :if => '!total_p_applied.nil?'
+  validates_presence_of :p_type_id, :if => '!total_p_applied.nil? && total_p_applied > 0'
 
   validates_presence_of :incorporation_date_year, :incorporation_date_month, :incorporation_date_day, :if => :is_incorporated?
   validates_numericality_of :incorporation_depth, :greater_than_or_equal_to => 0, :if => :is_incorporated?
