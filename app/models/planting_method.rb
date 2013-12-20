651e6c988bc31aff8cce272ab5423db5460e9054
Olivier <oleblond@allintel.com>
2013-12-20 16:28:09 -0500

    Bug fixes.

--- app/models/planting_method.rb
+++ app/models/planting_method.rb
@@ -1,6 +1,7 @@
 class PlantingMethod < ActiveRecord::Base
   # attr_accessible :title, :body
 
-  scope :for_cover_crops, where(:id < 500).order("name asc")
+  scope :for_cover_crops, where("id < 500").order("name asc")
+  #scope :for_cover_crops, lambda { where("id < 500") }
 
 end
