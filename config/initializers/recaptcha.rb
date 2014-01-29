4d7ddb10dab768d7bd43979adecff40dfc3a3ecf
kirstiecook <kirstiea@gmail.com>
2014-01-29 14:01:26 -0600

    update keys

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,5 +1,5 @@
 Recaptcha.configure do |config|
-  config.public_key  = '6Lehwu0SAAAAAMRIl38vDdobibvLgOH3UYlbk4cW'
-  config.private_key = '6Lehwu0SAAAAAL5wu7Fk_-lIrcZxGVrGnn90lhbo'
+  config.public_key  = '6Le0yO0SAAAAAMW-Ve1MsTCUMYsLqqeEpc_koyj5'
+  config.private_key = '6Le0yO0SAAAAAO96MtO5s8KnuGGdzwD6jGSr9Ao8'
   config.proxy = 'http://10.20.1.45:8001'
 end
