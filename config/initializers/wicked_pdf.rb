ef872f2e5202bf4b25f0dffa2e597d482db45b36
kirstiecook <kirstiea@gmail.com>
2014-02-03 12:07:24 -0600

    setup pdf

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,6 +1,8 @@
 if Rails.env.staging? || Rails.env.production?
   # Unix version
-  WickedPdf.config = { :exe_path => Rails.root.to_s + "/bin/wkhtmltopdf-amd64"  }
+  WickedPdf.config = {
+  :exe_path => Rails.root.join('bin', 'wkhtmltopdf-i386').to_s,
+}
 else
   # assume Windows environment
   WickedPdf.config = { :exe_path => 'C:\Program Files\wkhtmltopdf\wkhtmltopdf.exe'
