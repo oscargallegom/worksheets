72e1ab995baaae38223d9329cb4bbb38437afc7d
kirstiecook <kirstiea@gmail.com>
2014-02-03 08:45:14 -0600

    test

--- app/models/soil.rb
+++ app/models/soil.rb
@@ -14,8 +14,8 @@ class Soil < ActiveRecord::Base
   validates_inclusion_of :percent_clay, :in => 0..100, :message => 'must be between 0 and 100'
   validates_inclusion_of :percent_sand, :in => 0..100, :message => 'must be between 0 and 100'
 
-  validates_numericality_of :bulk_density, :greater_than_or_equal_to => 1.10, :less_than_or_equal_to => 1.79
-  validates_numericality_of :organic_carbon, :greater_than_or_equal_to => 0.50, :less_than_or_equal_to => 2.50
+  #validates_numericality_of :bulk_density, :greater_than_or_equal_to => 1.10, :less_than_or_equal_to => 1.79
+  #validates_numericality_of :organic_carbon, :greater_than_or_equal_to => 0.50, :less_than_or_equal_to => 2.50
 
   validates_inclusion_of :slope, :in => 0..100, :message => 'must be between 0 and 100'
 
