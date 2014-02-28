18f89e6a562f7888cdbf01f99c26a65f8c9c9adc
kirstiecook <kirstiea@gmail.com>
2014-02-28 10:54:56 -0600

    test

--- app/controllers/fields_controller.rb
+++ app/controllers/fields_controller.rb
@@ -60,6 +60,11 @@ class FieldsController < ApplicationController
   # GET /farms/1/fields/1/edit
   def edit
     @field = Field.find(params[:id])
+    @current_strips = @field.strips.where(:is_future => false)
+    @current_strips_rotations = @current_strips.crop_rotations
+    @future_strips = @field.strips.where(:is_future => true)
+    @future_strips_rotations = @future_strips.crop_rotations
+
     add_breadcrumb @farm.name, farm_path(@farm)
     add_breadcrumb 'Fields', farm_fields_path(@farm)
     add_breadcrumb @field.name
