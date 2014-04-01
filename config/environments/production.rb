d7e210197ff423d616d68551ccb75fe4fc2de739
kirstiecook <kirstiea@gmail.com>
2014-04-01 09:11:35 -0500

    testing forgot password bugfix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ NutrientNet::Application.configure do
 # }
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'http://cbntt.org' }
+  config.action_mailer.default_url_options = { :host => 'cbntt.org' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
