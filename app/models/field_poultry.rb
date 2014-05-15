33df59c6e2c6bcd84124a8441f9482d2901be08d
kirstiecook <kirstiea@gmail.com>
2014-05-15 08:50:55 -0500

    broke poultry

--- app/models/field_poultry.rb
+++ app/models/field_poultry.rb
@@ -3,9 +3,9 @@ class FieldPoultry < ActiveRecord::Base
   belongs_to :field #, :inverse_of => :field_poultry
   belongs_to :animal
 
-  attr_accessible :animal_id, :quantity, :flocks_per_year, :days_in_growing_cycle, :n_excreted, :p205_excreted
+  attr_accessible :poultry_id, :quantity, :flocks_per_year, :days_in_growing_cycle, :n_excreted, :p205_excreted
 
-  validates_presence_of :animal_id
+  validates_presence_of :poultry_id
   validates_numericality_of :quantity, :flocks_per_year, :n_excreted, :p205_excreted, :greater_than_or_equal_to => 0
   validates_numericality_of :days_in_growing_cycle, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 365
 
