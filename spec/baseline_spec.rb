f5d27b5d441320d139c7e04c89b3710b086ede0b
Kirstie Cook <kirstiea@gmail.com>
2014-10-17 18:11:20 +0200

    not touching prod db on test...

---
+++ spec/baseline_spec.rb
@@ -0,0 +1,29 @@
+require "spec_helper"
+
+describe Field do
+	include Baseline
+
+  	describe '#checkBaseline' do
+
+  		field = Field.new
+		strip = Strip.new
+		field.strips << strip
+		crop_rotation = CropRotation.new
+		strip.crop_rotations << crop_rotation
+		fert = ManureFertilizerApplication.new
+		crop_rotation.manure_fertilizer_applications << fert
+		fert.is_incorporated = false
+
+	    it "returns true if HEL soils and fertilizer NOT incorporated" do
+	  		field.hel_soils = true
+	  		assert_equal checkBaseline(field), true
+	  	end
+
+	  	it "returns false if HEL soils and fertilizer NOT incorporated" do
+	  		field.hel_soils = false
+	  		assert_equal checkBaseline(field), false
+	  	end
+
+  end
+
+end
