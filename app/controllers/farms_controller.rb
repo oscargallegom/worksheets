bf6d0c2a6ba30d732ae88827af93c946cd814050
Kirstie Cook <kirstiea@gmail.com>
2015-02-26 19:49:03 +0100

    added current review and removed calculate results buttons from current/future results pages

--- app/controllers/farms_controller.rb
+++ app/controllers/farms_controller.rb
@@ -248,6 +248,117 @@ class FarmsController < ApplicationController
     end
   end
 
+  def current
+
+    @completed = true
+
+    add_breadcrumb 'Projects', :farms_path
+    add_breadcrumb @farm.name
+
+@fields = Naturalsorter::Sorter.sort_by_method(@farm.fields, :name, true)
+
+    @watersheds = (@fields.collect {|x| x.watershed_segment}).uniq
+    @p_factors = ((@watersheds.collect {|z| z.p_delivery_factor.round(2)}).uniq).map {|i| i.to_s }.join(", ")
+    @n_factors = ((@watersheds.collect {|z| z.n_delivery_factor.round(2)}).uniq).map {|i| i.to_s }.join(", ")
+    @s_factors = ((@watersheds.collect {|z| z.sediment_delivery_factor.round(2)}).uniq).map {|i| i.to_s }.join(", ")
+    @arrWatersheds = Array.new
+    @arrMajorBasins = Array.new
+    @arrTMDLs = Array.new
+
+    @fields.each do |field|
+
+      @arrWatersheds << field.watershed_name unless @arrWatersheds.include?(field.watershed_name)
+      @arrMajorBasins << field.watershed_segment.major_basin unless field.watershed_segment.nil? || @arrMajorBasins.include?(field.watershed_segment.major_basin)
+      @arrTMDLs << field.tmdl.name if @farm.site_state_id != 47 && !field.tmdl.nil? && !@arrTMDLs.include?(field.tmdl.name)
+      @arrTMDLs << field.tmdl_va if @farm.site_state_id == 47 && !@arrTMDLs.include?(field.tmdl_va)
+
+    end
+
+    # check if the farm meets baseline or not
+    @baseline_n_load_fields = 0
+    @current_n_load_fields = 0
+    @future_n_load_fields = 0
+
+    @baseline_p_load_fields = 0
+    @current_p_load_fields = 0
+    @future_p_load_fields = 0
+
+    @baseline_sediment_load_fields = 0
+    @current_sediment_load_fields = 0
+    @future_sediment_load_fields = 0
+
+    @current_n_load_animals = 0
+    @current_p_load_animals = 0
+    @current_sediment_load_animals = 0
+
+    @future_n_load_animals = 0
+    @future_p_load_animals = 0
+    @future_sediment_load_animals = 0
+
+    @farm.fields.each do |field|
+      if (!field.field_type.nil?) && (field.field_type.id == 1 || field.field_type.id == 2 || field.field_type.id == 3)
+        begin
+          @current_totals = computeBmpCalculations(field)
+        rescue Exception => e
+          flash[:error] = e.message
+          @current_totals = {:new_total_n => 0, :new_total_p => 0, :new_total_sediment => 0, :new_total_n_future => 0, :new_total_p_future => 0, :new_total_sediment_future => 0}
+        end
+
+        @current_n_load_fields = @current_n_load_fields + @current_totals[:new_total_n]
+        @current_p_load_fields = @current_p_load_fields + @current_totals[:new_total_p]
+        @current_sediment_load_fields = @current_sediment_load_fields + @current_totals[:new_total_sediment]
+
+        logger.debug "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ @current_totals[:new_total_sediment] #{@current_totals[:new_total_sediment]}"
+        @new_total_sediment = @current_totals[:new_total_sediment]
+
+
+        @future_n_load_fields = @future_n_load_fields + @current_totals[:new_total_n_future]
+        @future_p_load_fields = @future_p_load_fields + @current_totals[:new_total_p_future]
+        @future_sediment_load_fields = @future_sediment_load_fields + @current_totals[:new_total_sediment_future]
+        @new_total_sediment_future = @current_totals[:new_total_sediment_future]
+
+        watershed_segment = WatershedSegment.where(:id => field.watershed_segment_id).first
+        if (!watershed_segment.nil?)
+          @baseline_sediment_load_fields += watershed_segment[:sediment_crop_baseline] * field.acres / 2000.0 if field.field_type_id == 1
+          @baseline_sediment_load_fields += watershed_segment[:sediment_pasture_baseline] * field.acres / 2000.0 if field.field_type_id == 2
+          @baseline_sediment_load_fields += watershed_segment[:sediment_hay_baseline] * field.acres / 2000.0 if field.field_type_id == 3
+
+
+          if field.tmdl.nil?
+            @baseline_n_load_fields += watershed_segment[:n_crop_baseline] * field.acres if field.field_type_id == 1
+            @baseline_n_load_fields += watershed_segment[:n_pasture_baseline] * field.acres if field.field_type_id == 2
+            @baseline_n_load_fields += watershed_segment[:n_hay_baseline] * field.acres if field.field_type_id == 3
+
+            @baseline_p_load_fields += watershed_segment[:p_crop_baseline] * field.acres if field.field_type_id == 1
+            @baseline_p_load_fields += watershed_segment[:p_pasture_baseline] * field.acres if field.field_type_id == 2
+            @baseline_p_load_fields += watershed_segment[:p_hay_baseline] * field.acres if field.field_type_id == 3
+
+          else # use Maryland TMDL
+            @baseline_n_load_fields += field.tmdl[:total_n] * field.acres
+            @baseline_p_load_fields += field.tmdl[:total_p] * field.acres
+          end
+        end
+      end
+      # animals
+      if (!field.field_type.nil?) && (field.field_type.id == 4)
+        @current_totals = computeLivestockBmpCalculations(field)
+        @current_n_load_animals += @current_totals[:current_load_nitrogen]
+        @current_p_load_animals += @current_totals[:current_load_phosphorus]
+        @current_sediment_load_animals += @current_totals[:current_load_sediment]
+        #future
+        @future_totals = computeLivestockBmpCalculationsFuture(field)
+        @future_n_load_animals += @future_totals[:current_load_nitrogen]
+        @future_p_load_animals += @future_totals[:current_load_phosphorus]
+        @future_sediment_load_animals += @future_totals[:current_load_sediment]
+      end
+
+      @is_farm_meets_baseline = is_farm_meets_baseline(@farm)
+
+    end
+  end
+
+
+
   # GET/farms/id/submit
   def submit
     add_breadcrumb 'Projects', :farms_path
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ NutrientNet::Application.routes.draw do
     get :send_to_mapping_site, on: :member
     post :receive_from_mapping_site, on: :member
     get :review, on: :member
+    get :current, on: :member
     get :submit, on: :member
   end
 
