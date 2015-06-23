c0bef1f32d03eee46ce80a8136d45229fe448a99
Kirstie Cook <kirstiea@gmail.com>
2015-06-23 22:01:20 +0200

    new url

--- config/initializers/global_parameters.rb
+++ config/initializers/global_parameters.rb
@@ -3,8 +3,8 @@
 #######################################################
 
 # URL to the mapping application
-#URL_MAP = ' http://ims.missouri.edu/NutrientNet/MultiStates.aspx'
-URL_MAP = ' http://nn.tarleton.edu/GoogleMapPost/Default.aspx'
+URL_MAP = ' http://projects.cares.missouri.edu/wri-nutrientnet/multistates.aspx'
+#URL_MAP = ' http://nn.tarleton.edu/GoogleMapPost/Default.aspx'
 URL_NTT = 'http://nn.tarleton.edu/nnmultiplestates/NNRestService.ashx'
 #### This is the test NTT ####
 #URL_NTT = 'http://nn.tarleton.edu/nntest1/NNRestService.ashx'
