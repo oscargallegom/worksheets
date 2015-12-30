d2ad099581f3417503afc01057d0df4641266e0e
Kirstie Cook <kirstiea@gmail.com>
2015-12-30 12:04:12 -0600

    add grazing info to current/review tabs

--- app/models/grazing_livestock.rb
+++ app/models/grazing_livestock.rb
@@ -12,4 +12,61 @@ class GrazingLivestock < ActiveRecord::Base
   #validates_numericality_of :end_date_day, :less_than_or_equal_to => 28, :if => 'end_date_month==2', :message => '^Date incorrect for February'
 
 
+  def display_start_month
+    if self.start_date_month == 1
+      "January"
+    elsif self.start_date_month == 2
+      "Februrary"
+    elsif self.start_date_month == 3
+      "March"
+    elsif self.start_date_month == 4
+      "April"
+    elsif self.start_date_month == 5
+      "May"
+    elsif self.start_date_month == 6
+      "June"
+    elsif self.start_date_month == 7
+      "July"
+    elsif self.start_date_month == 8
+      "August"
+    elsif self.start_date_month == 9
+      "September"
+    elsif self.start_date_month == 10
+      "October"
+    elsif self.start_date_month == 11
+      "November"
+    else
+      "December"
+    end
+  end
+
+
+   def display_end_month
+    if self.end_date_month == 1
+      "January"
+    elsif self.end_date_month == 2
+      "Februrary"
+    elsif self.end_date_month == 3
+      "March"
+    elsif self.end_date_month == 4
+      "April"
+    elsif self.end_date_month == 5
+      "May"
+    elsif self.end_date_month == 6
+      "June"
+    elsif self.end_date_month == 7
+      "July"
+    elsif self.end_date_month == 8
+      "August"
+    elsif self.end_date_month == 9
+      "September"
+    elsif self.end_date_month == 10
+      "October"
+    elsif self.end_date_month == 11
+      "November"
+    else
+      "December"
+    end
+  end
+
 end
