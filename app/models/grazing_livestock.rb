19a22378d25d0a422aaf6b2f4b00fe53835ef4c8
Kirstie Cook <kirstiea@gmail.com>
2016-09-06 08:47:13 -0600

    additional fields for grazing livestock

--- app/models/grazing_livestock.rb
+++ app/models/grazing_livestock.rb
@@ -2,7 +2,7 @@ class GrazingLivestock < ActiveRecord::Base
   belongs_to :crop_rotation
   belongs_to :animal
 
-  attr_accessible :start_date_year, :start_date_month, :start_date_day, :end_date_year, :end_date_month, :end_date_day, :animal_id, :animal_units, :hours_grazed, :precision_feeding
+  attr_accessible :start_date_year, :start_date_month, :start_date_day, :end_date_year, :end_date_month, :end_date_day, :animal_id, :animal_units, :hours_grazed, :precision_feeding, :consecutive_hours, :rest_time, :paddocks, :supplemental_feed, :round_bales
 
   validates_presence_of :start_date_year, :start_date_month, :start_date_day, :end_date_year, :end_date_month, :end_date_day, :animal_id
 
