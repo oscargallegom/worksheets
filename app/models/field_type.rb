eb0bb83b7cb9841474ebfd61ec64f8c9d85d5bee
Kirstie Cook <kirstiea@gmail.com>
2016-09-23 14:39:41 -0600

    limit scope of convertable field types

--- app/models/field_type.rb
+++ app/models/field_type.rb
@@ -3,4 +3,5 @@ class FieldType < ActiveRecord::Base
 
   has_many :field_type_bmp_types
   has_many :bmp_types, :through => :field_type_bmp_types
+  scope :convertable_fields, where(:id => [1,2,3])
 end
